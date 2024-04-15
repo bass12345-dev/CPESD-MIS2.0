ff387af367a0dcdec6319707a0ace21b4c0eb867
bass12345-dev <manabobasil@gmail.com>
2024-04-15 12:49:47 +0800

    update

--- storage/framework/views/2346a6e9db17b1911af92fca53aefd9ab5abd1a8.php
+++ storage/framework/views/2346a6e9db17b1911af92fca53aefd9ab5abd1a8.php
@@ -49,11 +49,11 @@
                 </a>
             </li>
 
-            <!-- <li class="sidebar-item <?= $segments[2] == 'outgoing' ? 'active' : '' ?>">
+            <li class="sidebar-item <?= $segments[2] == 'outgoing' ? 'active' : '' ?>">
                 <a class="sidebar-link" href="<?php echo e(url('/dts/user/outgoing')); ?>">
                     <i class="align-middle" data-feather="arrow-up"></i> <span class="align-middle">Outgoing</span>
                 </a>
-            </li> -->
+            </li>
 
             <li class="sidebar-item <?= $segments[2] == 'action-logs' ? 'active' : '' ?>">
                 <a class="sidebar-link" href="<?php echo e(url('/dts/user/action-logs')); ?>">
