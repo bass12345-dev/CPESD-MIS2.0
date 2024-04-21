f5c6e1759128d0e7222229e19bf697898ab337c7
bass12345-dev <manabobasil@gmail.com>
2024-04-21 17:47:29 +0800

    update

--- resources/views/watchlisted/users/contents/approved_list/approved.blade.php
+++ resources/views/watchlisted/users/contents/approved_list/approved.blade.php
@@ -20,8 +20,28 @@
       language: {
          "processing": '<div class="d-flex justify-content-center "> <img class="top-logo mt-4" src="{{asset("assets/img/peso_logo.png")}}"></div>'
       },
-      dom: 'Bfrtip',
-      buttons: ['copy', 'print', 'csv'],
+      "dom": "<'row'<'col-sm-12 col-md-4'l><'col-sm-12 col-md-4'B><'col-sm-12 col-md-4'f>>" + "<'row'<'col-sm-12'tr>>" + "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+      buttons: [
+            {
+               extend: 'copy',
+               text: 'Copy',
+               className: 'btn btn-warning rounded-pill ',
+               footer: true,
+               
+            }, 
+            {
+               extend: 'print',
+               text: 'Print',
+               className: 'btn btn-info rounded-pill  ms-2',
+               footer: true,
+              
+            }, {
+               extend: 'csv',
+               text: 'CSV',
+               className: 'btn btn-success rounded-pill ms-2',
+               footer: true,
+              
+            }, ],
       ajax: {
          url: base_url + "/wl/user/g-a-l",
          method: 'GET',
---
+++ storage/framework/views/99c0578596f623d632abe9adfa21852648c28919.php
@@ -0,0 +1,52 @@
+<div class="card flex-fill p-3">
+         <div class="card-header">
+            <h5 class="card-title mb-2">Documents</h5>
+         </div>
+         
+
+         <table class="table table-hover table-striped " id="datatables-buttons" style="width: 100%; "  >
+            <thead>
+               <tr>
+                 
+                  <th>#</th>
+                  <th>Program</th>
+                  <th>Program Description</th>
+                  <th>Created</th>
+                  <th>Actions</th>
+                 
+               </tr>
+            </thead>
+            <tbody>
+
+               <?php $i = 1;  foreach ($programs as $row) : ?>
+
+                <tr>
+                       
+                        <td><?php echo $i++; ?></td>
+                        <td><?php echo $row->program; ?></td>
+                        <td><?php echo $row->program_description; ?></td>
+                        <td><?php echo $row->created; ?></td>
+                        <td> 
+                           <div class="btn-group dropstart">
+                             <i class="fa fa-ellipsis-v " class="dropdown-toggle"  data-bs-toggle="dropdown" aria-expanded="false"></i>
+                             <ul class="dropdown-menu">
+                                  <li><a class="dropdown-item" id="update" href="javascript:;" data-id="<?php echo e($row->program_id); ?>" data-name="<?php echo e($row->program); ?>" data-description="<?php echo e($row->program_description); ?>">Update</a></li>
+                                  <li><a class="dropdown-item" href="#" id="remove" href="javascript:;" data-id="<?php echo e($row->program_id); ?>">Remove</a></li>
+                                </ul>
+                           </div>                 
+                        </td>
+                       
+                     </tr>
+
+
+               <?php endforeach; ?>
+               
+                  
+            </tbody>
+         </table>
+      </div>
+
+
+
+
+<?php /**PATH C:\xampp\htdocs\CPESD-MIS2.0\resources\views/watchlisted/admin/contents/manage_program/sections/program_table.blade.php ENDPATH**/ ?>
